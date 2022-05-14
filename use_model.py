794104daa9eecfca622baa0af9d9afaf323ea466
Yuanhao-JIANG <71644411+Yuanhao-JIANG@users.noreply.github.com>
2022-05-14 20:41:06 +0100

    refactor

--- use_model.py
+++ use_model.py
@@ -6,30 +6,33 @@ from torchvision import transforms
 from model import LeNet5
 
 
-img_batch = []
-img_paths = glob.glob("./imgs/*")
-for i in range(len(img_paths)):
-    img = Image.open(img_paths[i]).convert("L")
-    x = transforms.ToTensor()(img)
-    img_batch.append(x.numpy())
+def recognize(img_dir="./imgs", output_path="./output.txt", model_path="./saved_model/model.pt"):
+    img_batch = []
+    img_paths = glob.glob(img_dir+"/*")
+    for i in range(len(img_paths)):
+        img = Image.open(img_paths[i]).convert("L")
+        x = transforms.ToTensor()(img)
+        img_batch.append(x.numpy())
 
-# convert to numpy array first to make the following transformation to tensor faster
-img_batch = numpy.array(img_batch)
-img_batch = torch.tensor(img_batch)
+    # convert to numpy array first to make the following transformation to tensor faster
+    img_batch = numpy.array(img_batch)
+    img_batch = torch.tensor(img_batch)
 
-device = "cuda" if torch.cuda.is_available() else "cpu"
+    device = "cuda" if torch.cuda.is_available() else "cpu"
 
-model = LeNet5(10)
-model.to(device)
-model.load_state_dict(torch.load("./saved_model/model.pt"))
-model.eval()
+    model = LeNet5(10)
+    model.to(device)
+    model.load_state_dict(torch.load(model_path))
+    model.eval()
 
-x = img_batch.to(device)
-with torch.no_grad():
-    outputs, _ = model(x)
-    outputs = outputs.detach().cpu()
+    x = img_batch.to(device)
+    with torch.no_grad():
+        outputs, _ = model(x)
+        outputs = outputs.detach().cpu()
 
-    with open("./output.txt", "w") as f:
-        for i in range(len(outputs)):
-            f.write(f"{img_paths[i]} -> {torch.argmax(outputs[i]).item()}" + "\n")
+        with open(output_path, "w") as f:
+            for i in range(len(outputs)):
+                f.write(f"{img_paths[i]} -> {torch.argmax(outputs[i]).item()}" + "\n")
 
+
+# recognize()
